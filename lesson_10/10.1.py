f3840b0a5bb72574c7cf74849db9f0211f1eabf6
mykhailo.stakhiv <mykhailo.stakhiv@infotech.gov.ua>
2026-04-30 12:57:31 +0300

    homework_10: fix OOP diamond inheritance

--- lesson_10/10.1.py
+++ lesson_10/10.1.py
@@ -4,29 +4,24 @@ import math
 # Завдання 1
 
 class Employee:
-    def __init__(self, name, salary, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, name, salary):
         self.name = name
         self.salary = salary
 
 class Manager(Employee):
-    def __init__(self, department, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, name, salary, department):
+        Employee.__init__(self, name, salary)
         self.department = department
 
 class Developer(Employee):
-    def __init__(self, programming_language, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, name, salary, programming_language):
+        Employee.__init__(self, name, salary)
         self.programming_language = programming_language
 
 class TeamLead(Manager, Developer):
     def __init__(self, name, salary, department, programming_language, team_size):
-        super().__init__(
-            name=name,
-            salary=salary,
-            department=department,
-            programming_language=programming_language,
-        )
+        Manager.__init__(self, name, salary, department)
+        Developer.__init__(self, name, salary, programming_language)
         self.team_size = team_size
 
 def test_team_lead_attributes():
