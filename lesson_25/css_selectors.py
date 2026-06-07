a044228cdfda08b6a0f3e465183883929fb406e9
Михайло <m.stakhiv11@gmail.com>
2026-06-07 10:34:56 +0300

    Add XPath and CSS locators content

---
+++ lesson_25/css_selectors.py
@@ -0,0 +1,52 @@
+# HEADER
+HEADER_LOGO = "a.header_logo"
+SIGN_IN_BUTTON = "button.header_signin"
+GUEST_LOGIN_LINK = "button.header-link.-guest"
+HOME = "a[routerlink='/']"
+ABOUT = "button[appscrollto='aboutSection']"
+CONTACTS = "button[appscrollto='contactsSection']"
+
+
+# SIGN IN MODAL
+BUTTON_CLOSE = "button[aria-label='Close']"
+EMAIL = "#signinEmail"
+PASSWORD = "#signinPassword"
+FORGOT_PASSWORD_LINK = "app-signin-modal .form-check + button"
+REGISTRATION = "app-signin-modal .modal-footer button.btn-link"
+LOGIN_BUTTON = "app-signin-modal .modal-footer button.btn-primary"
+
+
+# CONTACTS
+FACEBOOK_LINK = "a[href*='facebook']"
+TELEGRAM_LINK = "a[href*='t.me']"
+YOUTUBE_LINK = "a[href*='youtube']"
+INSTAGRAM_LINK = "a[href*='instagram']"
+LINKEDIN_LINK = "a[href*='linkedin']"
+ITHILLEL_LINK = "a.contacts_link"
+SUPPORT_LINK = "a[href*='mailto']"
+
+
+# GARAGE PAGE
+ADD_BUTTON = ".panel-page_heading button.btn-primary"
+MY_PROFILE = "#userNavDropdown"
+
+
+# SIDEBAR
+GARAGE_SIDEBAR = "a.sidebar_btn[routerlink='garage']"
+FUEL_EXPENSES_SIDEBAR = "nav.sidebar a[routerlink='expenses']"
+INSTRUCTIONS_SIDEBAR = "nav.sidebar a[routerlink='instructions']"
+LOG_OUT_BUTTON = "a.text-danger"
+
+
+# ADD A CAR MODAL
+ADD_CAR_BRAND = "#addCarBrand"
+ADD_CAR_MODEL = "#addCarModel"
+ADD_CAR_CANCEL_BUTTON = "app-add-car-modal button.btn-secondary"
+ADD_CAR_ADD_BUTTON = "app-add-car-modal button.btn-primary"
+
+
+# MY PROFILE DROPDOWN
+PROFILE_GARAGE = "nav.user-nav_menu a[routerlink='/panel/garage']"
+PROFILE_FUEL_EXPENSES = "nav.user-nav_menu a[routerlink='/panel/expenses']"
+PROFILE_INSTRUCTIONS = "nav.user-nav_menu a[routerlink='/panel/instructions']"
+PROFILE_LOGOUT = "nav.user-nav_menu button.user-nav_link"
